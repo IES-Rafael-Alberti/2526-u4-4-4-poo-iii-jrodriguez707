887af7d4c827d442dd67c892f17d81e8a85788f7
jrodriguez707 <jrodriguezr@iesrafaelalberti.es>
2026-01-25 22:04:51 +0100

    .

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,16 +1,71 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+enum class Direccion {
+    PositiveX,
+    NegativeX,
+    PositiveY,
+    NegativeY
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class Robot(
+    val nombre: String
+) {
+    var posicionX: Int = 0
+    var posicionY: Int = 0
+    var direccion: Direccion = Direccion.PositiveY
+
+    init {
+        require(nombre.isNotBlank())
+    }
+
+    fun mover(pasos: IntArray) {
+        for (paso in pasos) {
+            when (direccion) {
+                Direccion.PositiveY -> posicionY += paso
+                Direccion.NegativeY -> posicionY -= paso
+                Direccion.PositiveX -> posicionX += paso
+                Direccion.NegativeX -> posicionX -= paso
+            }
+            girar()
+        }
+    }
+
+    private fun girar() {
+        direccion = when (direccion) {
+            Direccion.PositiveY -> Direccion.NegativeX
+            Direccion.NegativeX -> Direccion.NegativeY
+            Direccion.NegativeY -> Direccion.PositiveX
+            Direccion.PositiveX -> Direccion.PositiveY
+        }
+    }
+
+    fun obtenerPosicion(): Pair<Int, Int> {
+        return Pair(posicionX, posicionY)
+    }
+
+    fun obtenerDireccion(): String {
+        return direccion.name
+    }
+
+    override fun toString(): String {
+        return "$nombre está en ($posicionX, $posicionY) ${obtenerDireccion()}"
+    }
+}
+
+fun main() {
+
+    val robot1 = Robot("R2D2")
+
+    val movimientos = arrayOf(
+        intArrayOf(1, -5, 0, -9),
+        intArrayOf(3, 3, 5, 6, 1, 0, 0, -7),
+        intArrayOf(2, 1, 0, -1, 1, 1, -4),
+        intArrayOf(),
+        intArrayOf(3, 5)
+    )
+
+    for (movimiento in movimientos) {
+        robot1.mover(movimiento)
+        println(robot1)
     }
 }
